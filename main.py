bcd815c18de292b5d9b2ab11a9262d6254b269ca
sbenshalom <sbenshalom@microsoft.com>
2025-04-11 12:55:34 +0300

    Initial commit: Weather API project with FastMCP server

---
+++ main.py
@@ -0,0 +1,8 @@
+from weather import mcp
+
+if __name__ == "__main__":
+    print("Server started1\n")
+    # Initialize and run the server
+    mcp.run(transport='stdio')
+    print("Server started")
+    
